46b6608b3c631adce28971ed1596364355920bbf
Quelmy <Ferreirariquelmy27@gmail.com>
2025-08-28 09:06:17 -0300

    Update layout.tsx
    remoção de importação de fonte local

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import { Lato } from "next/font/google";
 import { Header } from "../components/Header";
@@ -8,18 +7,6 @@ const lato = Lato({
   subsets: ['latin'],
   weight: ['400', '700']
 });
-
-// Se você não vai usar essas fontes ainda, pode comentar/remover
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
